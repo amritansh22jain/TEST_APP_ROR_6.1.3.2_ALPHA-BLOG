cff6ae68c26ca3b88ac6c9d1a45625f02fc473d6
Amritansh Jain <amitansh.jain@gmail.com>
2021-07-08 21:03:20 +0530

    Added the application helper methods to application controller as helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+#This method will tell us the currently signed_in User
+  def current_user
+    #This increases the efficiency by not hitting the database evertime to find the User
+    @current_user ||= User.find(session[:user_id]) if session[:user_id] #memoization logical OR operator
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
